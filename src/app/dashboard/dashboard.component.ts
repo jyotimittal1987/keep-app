72cff04a06d1f300caacfb06c47cc09e616d0961
jyotimittal1987 <jyotimittal1987@gmail.com>
2022-08-17 12:58:29 +0530

    Add files via upload

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../models/note';
+import { NOTES } from '../models/notes';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  notes: Note[] = NOTES;
+  note: Note = {};
+  searchText: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  searchNote(){
+    this.notes = NOTES;
+    if(this.searchText !== "")
+      this.notes = this.notes.filter(note => note?.title?.includes(this.searchText));
+  }
+  
+  addNote() {
+    if(this.note)
+      if(this.note.id !== 0 && this.note.title !== ""){
+        NOTES.push(this.note);
+        this.note = {};
+      }
+    
+  }
+}
